9cae42d69d14ac02007d87815b86a245e43ed790
Hackersploit__xxSploitation <78088921+hackersploitxxsploitation@users.noreply.github.com>
2021-04-30 17:49:25 -0300

    Add files via upload

---
+++ scareware.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <windows.h>
+ 
+
+// scareware 
+
+int main()
+{
+  MessageBox (NULL,"SEU PC FOI INFECTADO!!!","Para resolver acesse o link ao lado...",MB_OK );
+  
+  return 0;
+}
